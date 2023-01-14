0916a53cb93261242373a952ee73ec86fb802028
Ankur Solanki <ankursolanki1537@gmail.com>
2023-01-14 12:02:55 +0530

    main page structure

--- src/App.js
+++ src/App.js
@@ -4,10 +4,63 @@ import { useState } from 'react'
 
 function App() {
 
+  const dummy = [{
+    reponame: "Heroes",
+    description:"Heroes description goes here",
+    tags:["javascript","angular","angular.js","Angular 2+"]
+  },{
+    reponame: "Heroes",
+    description:"Heroes description goes here",
+    tags:["javascript","angular","angular.js","Angular 2+"]
+  },{
+    reponame: "Heroes",
+    description:"Heroes description goes here",
+    tags:["javascript","angular","angular.js","Angular 2+"]
+  },{
+    reponame: "Heroes",
+    description:"Heroes description goes here",
+    tags:["javascript","angular","angular.js","Angular 2+"]
+  }]
+
   return (
-    <>
-      
-    </>
+    <Grid container>
+      <Grid container>
+        <Grid item>
+          {/* image */}
+        </Grid>
+        <Grid item container direction = "column">
+          <Grid item>
+            {/* Name */}
+          </Grid>
+          <Grid item>
+            {/* Bio */}
+          </Grid>
+          <Grid item container>
+            <Grid item>
+              {/* location image */}
+            </Grid>
+            <Grid item>
+              {/* location name */}
+            </Grid>
+          </Grid>
+          <Grid item>
+            {/* Twitter */}
+          </Grid>
+        </Grid>
+      </Grid>
+      {/* github link */}
+      <Grid item container>
+        <Grid item>
+          {/* link image */}
+        </Grid>
+        <Grid item>
+          {/* link */}
+        </Grid>
+      </Grid>
+      <Grid item container>
+        
+      </Grid>
+    </Grid>
   );
 }
 
