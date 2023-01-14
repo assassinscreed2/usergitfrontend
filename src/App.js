cc6a98b250e1248cc2b5f8b31c6c6485e7bdec9f
Ankur Solanki <ankursolanki1537@gmail.com>
2023-01-14 11:31:27 +0530

    asking page working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {Grid,useMediaQuery,Typography,TextField, Button} from '@mui/material'
+import {useTheme} from '@mui/material/styles'
+import { useState } from 'react'
 
 function App() {
+
+  const theme = useTheme();
+  const matchesMD = useMediaQuery(theme.breakpoints.down('md'))
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container alignItems="center" justifyContent="center" style={{minHeight:"100vh"}}>
+      <Grid item justifyContent="center" alignContent="space-around" container style={{minHeight:"10em",border:"8px solid #2DA44E",width:"20em",borderRadius:"5%"}}>
+        <Grid item >
+          <Typography align="center" style={{fontWeight:"900"}} color="#2DA44E">
+            Enter Github Username
+          </Typography>
+        </Grid>
+        <Grid item style={{width:"90%"}}>
+          <TextField size="small" style={{width:"100%"}}/>
+        </Grid>
+        <Grid item>
+          <Button variant='outlined' style={{color:"#2DA44E",borderColor:"#2DA44E"}}>Submit</Button>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
